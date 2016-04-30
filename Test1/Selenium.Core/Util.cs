37d5bfbad044260610465126c7636d13d397110b
Krishna Prasad <krishna.prasad@nordstrom.com>
2016-04-29 18:34:48 -0600

    Base framework

---
+++ Test1/Selenium.Core/Util.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+
+namespace Selenium.Core
+{
+    public static class Util
+    {
+
+
+        public static string GetValue(this IWebElement webElement)
+        {
+            IJavaScriptExecutor e = Browser.Instance as IJavaScriptExecutor;
+            return (string)e.ExecuteScript(string.Format("return $('#{0}').val();", webElement.GetAttribute("id")));
+        }
+
+    }
+}
---
+++ Test1/Selenium.Specifications/Hooks/StepsBase.cs
@@ -0,0 +1,45 @@
+﻿using Selenium.Core;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Specifications.Hooks
+{
+    public static class StepsBase
+    {
+        private static readonly string localURL = "http://127.0.0.1:7777/portalserver/localhost/activate";
+        private static readonly string devURL = "https://localhostcarddev/portalserver/localhost/index";
+        private static readonly string testURL = "https://localhostcardtest.localhost.net/portalserver/localhost/index";
+        private static readonly string prodURL = "https://norstromcard.com/portalserver/localhost/index";
+
+        public static void NavigateTo()
+        {
+            Browser.Instance.Url = GetURL();
+            WebDriverWait wait = new WebDriverWait(Browser.Instance, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.TitleContains(Browser.Instance.Title));
+        }
+
+        private static string GetURL()
+        {
+            string environment = ConfigurationManager.AppSettings["Environment"];
+
+            switch (environment)
+            {
+                case "Local":
+                    return localURL;
+                case "Development":
+                    return devURL;
+                case "Test":
+                    return testURL;
+                case "Production":
+                    return prodURL;
+                default:
+                    return null;
+            }
+        }
+    }
+}
